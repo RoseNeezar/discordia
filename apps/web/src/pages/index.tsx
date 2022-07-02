69d45f3a6dd21b269c256654c29384666e68d610
Neezar <rforlyffe@gmaicl.com>
2022-07-02 23:49:45 +0800

    update to pnpm

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -3,8 +3,14 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1 className="bg-green-700 text-white">Discord app</h1>
-
+      <h1 className="bg-red-400">Discord app</h1>
+      <a
+        href={`https://api.whatsapp.com/send?phone=+60104016848&text=${
+          "Hello ! here is text ! click " + "https://tinyurl.com/36kedumb"
+        }`}
+      >
+        Click
+      </a>
       <Button />
     </div>
   );
---
+++ packages/typings/auth/types/index.ts
@@ -0,0 +1,8 @@
+interface IUser {
+  _id: any;
+  email: string;
+  username: string;
+  password?: string;
+}
+
+export type { IUser };
